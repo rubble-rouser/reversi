38177f3f671a5872b2e78b10a435b6f95ba771a8
Kenan Yildirim <kenan@kenany.me>
2015-04-18 13:15:14 -0400

    Document `Board::Board`

--- src/board.cpp
+++ src/board.cpp
@@ -1,5 +1,11 @@
 #include "board.h"
 
+// Internal representation of the state of the board.
+//
+//   -1 = valid move (as determined by `Game::possibleMoves`)
+//    0 = empty space, invalid move
+//    1 = white piece
+//    2 = black piece
 Board::Board() {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
@@ -7,6 +13,9 @@ Board::Board() {
     }
   }
 
+  // Initial board begins with 2 blacks and 2 whites at the center. Blacks are
+  // placed at north-west and south-east. Whites are placed at north-east and
+  // south-west.
   board[3][3] = 2;
   board[4][4] = 2;
   board[3][4] = 1;
